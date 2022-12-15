8c7327052125e7fd6866247b545fd1748c1e52a8
Patrick Kariuki <patmangu2018@gmail.com>
2022-12-14 22:46:29 -0500

    Added StdDraw library and project 1 starter file to directory

---
+++ project-1/Project1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/** Starter file for Project A, EN.500.112 Gateway Computing: Java.
+* Mondrian Art
+* Fall 2022
+* @author Joanne Selinski
+* @author <STUDENT TO DO: FILL IN YOUR NAME and JHED HERE!>
+*/
+public class Project1 {
+
+   /** Main method.
+   * @param args not used
+   */
+   public static void main(String[] args) {
+   
+      Scanner kb = new Scanner(System.in);  // this allows us to collect input
+   
+      // variable declarations
+
+   
+      // fixed-value "constants" needed by the program
+      final int SIZE = 512;  // can be increase if you want
+      StdDraw.setCanvasSize(SIZE, SIZE);
+    
+      // Prompt for and collect parameters from the user
+   
+      // Make the output based on the inputs
+      
+   }
+}
